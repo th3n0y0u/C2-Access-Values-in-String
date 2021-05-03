18dd40ae18b66555a813598d4f6e75aef9374952
soop <>
2021-05-03 23:28:55 +0000

    code

---
+++ main.py
@@ -0,0 +1,35 @@
+'''
+
+Access values in String
+- To access substrings, Use the square bracets for slicing along with index or indixies to obtain your substring.
+
+var = "Hello"
+
+# Individual
+string: | H | e | l | l | o |
+positive: | 0 | 1 | 2 | 3 | 4 |
+negative: | -5 | -4 | -3 | -2 | -1 |
+
+# Consective
+string: | H | e | l | l | o |
+index: 0   1   2   3   4   5
+
+In case of printing consecutive characters, it needs to strat beginning index and colon, then ending index
+
+ex) print "ello" from var
+print (var[1: ])
+
+'''
+
+List1 = ["a", "b", "g", "y", "j", "k", "h"]
+print(List1[1:4])
+
+string = "student"
+print(string[3:])
+
+var1 = "Hello World!"
+var2 = "Python Programming"
+# 1. print 'h' from var2
+print(var2[3])
+# 2. print 'hi' from using var1, and var2
+print(var1[0] + var2[-3])
